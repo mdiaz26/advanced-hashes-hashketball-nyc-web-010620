268ea8445754503476184a9f0b0af0d8cbdcdc37
Michael Diaz <mike.diaz006@gmail.com>
2019-12-22 14:15:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -148,8 +148,8 @@ end
 def long_name_steals_a_ton?
   most_steals = combine_players(game_hash).reduce do |memo, hash|
     memo[:steals] > hash[:steals] ? memo : hash
-  player_with_longest_name == most_steals[:player_name] ?
 end
+  player_with_longest_name == most_steals[:player_name] ?
 
 
 
