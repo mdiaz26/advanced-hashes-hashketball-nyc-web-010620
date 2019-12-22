853b3cbed74aee4c2ab7ad8dc3ab5e94ba0d3a43
Michael Diaz <mike.diaz006@gmail.com>
2019-12-22 13:19:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -112,7 +112,7 @@ end
 
 def big_shoe_rebounds
   biggest = combine_players(game_hash).reduce do |memo, hash|
-    memo[:shoe] < hash[:shoe] ? memo : hash
+    memo[:shoe] > hash[:shoe] ? memo : hash
   end
   biggest
   binding.pry
